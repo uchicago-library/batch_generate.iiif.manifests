d663e24ccdbfd6426fcf7ea9623443b34e2bcf51
verbalhanglider <tdanstrom@uchicago.edu>
2018-03-29 14:37:53 -0500

    added blank tests just to make it work with travis.yml

---
+++ tests/test_modules.py
@@ -0,0 +1,11 @@
+import unittest
+
+class Tests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testPass(self):
+        self.assertEqual(True, True)
